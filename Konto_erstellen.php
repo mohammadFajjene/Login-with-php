d1479457773109ac6698a3cdd35859c7ed8bd3f6
Mohammad Deeb Fajjene <72527610+mohammadFajjene@users.noreply.github.com>
2022-11-09 12:53:27 +0100

    init

---
+++ Konto_erstellen.php
@@ -0,0 +1,257 @@
+<?php
+ $host ='localhost';
+ $username ='root';
+ $password ='';
+ $dbname ='test1';
+ 
+
+
+ $conn= mysqli_connect( 'localhost' , 'root' , '' , 'teilnehmer');
+
+
+ if (!$conn) {
+     echo mysqli_connect_error("false Verbindung") .mysqli_connect_errno();
+    #die("connection error:"'. mysqli_connect_error()');
+     # code...
+ }
+
+  
+  
+
+ if (isset($_POST['submit'])) {
+     
+    
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $mail = mysqli_real_escape_string($conn,$_POST['email'] );
+    $pass = mysqli_real_escape_string($conn,$_POST['password'] );
+
+    $query_mail= "select * from information where email='$mail'";
+    $result= mysqli_query($conn, $query_mail);
+    $test= mysqli_num_rows($result);
+
+    
+     if ($test==0 && $mail != 0) { 
+
+     $sql = "INSERT INTO  information (firstname,nachname, email, password)
+         VALUES ('$firstname', '$lastname' ,'$mail ','$pass')";
+
+     $rs = mysqli_query($conn, $sql);
+
+        if ($rs) {
+            echo "<center><p class='mystyle1'>Successfuly.Jetzt können Sie Anmelden<br></p></center>";
+      
+        }else {
+           echo "error:".$sql."<br>".mysqli_error($conn);}
+         }else {
+            echo "<center><p class='mystyle2'>E-Mail bereit existiert!.</p></center>";
+        }
+    
+    }
+
+mysqli_close($conn);
+
+?>
+<style>
+    @media only screen and (max-width: 400px) {
+        .mystyle1{
+        margin: 0;
+        color:green;
+        transform: translate(0px,740px);
+        font-size: large;
+        position: absolute;
+        
+    }
+    .mystyle2{
+        margin: 0;
+        color:red;
+        transform: translate(0px,740px);
+        font-size: large;
+        
+        
+    }}
+   
+     @media only screen and (min-width: 400px) {
+        .mystyle1{
+        margin: 0;
+        color:green;
+        transform: translate(0px,780px);
+        font-size: large;
+        position: absolute;
+        
+    }
+    .mystyle2{
+        margin: 0;
+        color:red;
+        transform: translate(0px,780px);
+        font-size: large;
+        
+        
+    }}
+    @media only screen and (min-width: 600px) {
+        .mystyle1{
+        margin: 0;
+        color:green;
+        transform: translate(0px,700px);
+        font-size: large;
+        position: absolute;
+        
+    }
+    .mystyle2{
+        margin: 0;
+        color:red;
+        transform: translate(0px,600px);
+        font-size: large;
+        
+        
+    }}
+    @media only screen and (min-width: 768px) {
+        .mystyle1{
+        margin: 0;
+        color:green;
+        transform: translate(0px,750px);
+        font-size: large;
+        position: absolute;
+        
+    }
+    .mystyle2{
+        margin: 0;
+        color:red;
+        transform: translate(0px,750px);
+        font-size: large;
+        
+        
+    }}
+    @media only screen and (min-width: 820px) {
+    .mystyle1{
+        margin: 0;
+        color:green;
+        transform: translate(0px,800px);
+        font-size: large;
+        position: absolute;
+        
+    }
+    .mystyle2{
+        margin: 0;
+        color:red;
+        transform: translate(0px,800px);
+        font-size: large;
+        
+        
+    }}
+    @media only screen and (min-width: 992px) {
+    .mystyle1{
+        margin: 0;
+        color:green;
+        transform: translate(0px,730px);
+        font-size: large;
+        position: absolute;
+        
+    }
+    .mystyle2{
+        margin: 0;
+        color:red;
+        transform: translate(0px,730px);
+        font-size: large;
+        
+        
+    }}
+</style>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Konto erstellen</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="konto.css">
+    
+</head>
+<body>
+    <center><img name="foto" class="foto" src="foto/logo.png" alt="user"> </center>
+    <div class="signup-box"> 
+        <div class="one">
+        <h1 >Sign Up</h1><h3 >Herzlich Welcomen !</h3>
+            
+        </div>
+        
+
+  
+  
+    <form class="container" action="" method="POST">
+    
+        <div class="name">
+        <label class="geschlecht">Geschlecht : </label>              
+        <label for="man" class="woman"> weiblich</label><input id="man" type="radio" name="gender">
+        <label for="woman" class="man">mänlich</label><input id="woman" type="radio" name="gender">    
+        </div>
+
+        <label for="firstlabel" class="vorname">VorName:</label>
+        <label id="alertme1"></label><br>
+        <input id="firstlabel" class="firstlabel" name="firstname" onclick="stop()" type="text" placeholder=""><br>
+        
+        
+        <label for="lastlabel" class="nachname">NachName:</label>
+        <label id="alertme2"></label> <br>
+        <input id="lastlabel" class="lastlabel" name="lastname" onclick="stop()" type="text" placeholder="" ><br>
+
+        <label for="emaillabel" class="email">E-Mail:</label>
+        <label id="alertme3"></label><br>
+        <input id="emaillabel" class="emaillabel" name="email" onclick="stop()" type="email" placeholder="" required><br>
+
+        <label for="passwordlabel" class="password">Passwort:</label>
+        <label id="alertme4"></label><br>                       
+        <input id="passwordlabel" class="passwordlabel" name="password" onclick="stop()" type="password" placeholder="" ><br>
+
+        <label  for="wiederholenlabel" class="wiederholen">Passwort bestätigen:</label><br>
+        <input id="wiederholenlabel" class="wiederholenlabel" onclick="stop()" type="password" placeholder=""><br><br><br>
+
+        <input class="erstellen" type="submit" name="submit" onclick="run()" value="Erstellen"><br><br>
+    </form>
+    
+    <div class="text-center">
+        <p >Sie haben bereits ein Konto?<a href="http://localhost/index/login.php" >   Login hier</a></p>
+        </p>
+      </div>
+    
+
+    </div>
+
+    <script>
+      function run(){
+        var vorname= document.getElementById("firstlabel").value;
+        var nachname=document.getElementById("lastlabel").value;
+        var email=document.getElementById("emaillabel").value;
+        var password=document.getElementById("passwordlabel").value;
+        var password2=document.getElementById("wiederholenlabel").value;
+
+
+        if (vorname=="") {
+            document.getElementById("alertme1").innerHTML="Bitte Vorname eingeben";
+            document.getElementById("alertme1").style.color="red";
+        }else if (nachname == "") {
+            document.getElementById("alertme2").innerHTML="Bitte Nachname eingeben";
+            document.getElementById("alertme2").style.color="red";
+        }else if (email == "") {
+            document.getElementById("alertme3").innerHTML="Bitte E-Mail eingeben";
+            document.getElementById("alertme3").style.color="red";
+        }else if (password == "" || password2 == "" || password !=password2) {
+            document.getElementById("alertme4").innerHTML="Password stimmt nicht überein";
+            document.getElementById("alertme4").style.color="red";
+        }
+        }
+        function stop(){
+            document.getElementById("alertme1").innerHTML="";
+            document.getElementById("alertme2").innerHTML="";
+            document.getElementById("alertme3").innerHTML="";
+            document.getElementById("alertme4").innerHTML="";
+        }
+      
+    </script>
+    
+    
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,166 @@
+
+<?php
+$host ='localhost';
+$username ='root';
+$password ='';
+$dbname ='test1';
+
+
+$conn= mysqli_connect( 'localhost' , 'root' , '' , 'teilnehmer');
+
+
+if (!$conn) {
+    echo mysqli_connect_error("false Verbindung") .mysqli_connect_errno();
+   #die("connection error:"'. mysqli_connect_error()');
+    # code...
+}
+if (isset($_POST['submit'])) {
+$mail = mysqli_real_escape_string($conn,$_POST['email'] );
+$pass = mysqli_real_escape_string($conn,$_POST['password'] );
+
+$query= "select * from information where email='$mail' and password ='$pass'";
+$result= mysqli_query($conn, $query);
+$test= mysqli_num_rows($result);
+
+$query_mail= "select * from information where email='$mail'";
+$result_mail= mysqli_query($conn, $query_mail);
+$test_mail= mysqli_num_rows($result_mail);
+if ($test>0) {
+  header("Location: https://myvapez.de/collections/");
+  exit;
+  }if ($test_mail>0) {
+      echo "<p class='mystyle'>E-Mail und Password stimmt nicht überein</p>" ;
+   }else{
+    echo "<p class='mystyle'>kein Konto mit diesem E-Mail</p>" ; "";
+   }
+
+}
+
+mysqli_close($conn);
+
+
+?>
+<style>
+ @media only screen and (max-width: 600px) {
+    .mystyle{
+    font-size:large;
+    margin: 0%;
+    position: absolute;
+    color:red;
+    letter-spacing: 1px;
+    top: 77%;
+    left: 35%;
+    transform: translate(-50%, 0%); 
+    
+     
+    }}
+ @media only screen and (min-width: 600px) {
+    .mystyle{
+    font-size:large;
+    margin: 0%;
+    position: absolute;
+    color:red;
+    letter-spacing: 1px;
+    top: 50%;
+    left: 51%;
+    transform: translate(-50%, 0%); 
+    
+      
+     
+    }}
+ @media only screen and (min-width: 768px) {
+    .mystyle{
+    font-size:x-large;
+    margin: 0%;
+    position: absolute;
+    color:red;
+    letter-spacing: 1px;
+    top: 50%;
+    left: 51%;
+    transform: translate(-50%, 0%); 
+    
+     
+     
+    }}
+@media only screen and (min-width: 992px) {
+    .mystyle{
+    font-size:x-large;
+    margin: 0%;
+    position: absolute;
+    color:red;
+    letter-spacing: 1px;
+    top: 74%;
+    left: 51%;
+    transform: translate(-50%, 0%); 
+    
+        
+    }
+
+
+}
+
+
+</style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="logo.css">
+    <title>Login</title>
+</head>
+<body>
+    
+
+    
+    <header>
+        
+        <img src="foto/logo.png" alt="user">  
+        
+    </header>
+   
+    
+         <section class="example">
+            <div class="log">
+            <h1 class="text" >Login</h1>
+            <a class="erstellen" href="http://localhost/index/konto_erstellen.php">Registieren</a>
+            </div>
+            <form name="form" action="" method="post">
+          <br> <br>
+            <input id="email" class="email" name="email" onclick="hold()" type="text" require placeholder="E-Mail Adresse">
+          <br><br><br>
+            <input id="pass" class="pass" name="password" onclick="hold()" type="password"  placeholder="Enter Password" ><br><br>
+            <label id="alert"></label><br><br><br>
+        
+            <button class="main-btn" type="submit" name="submit" onclick="Alert()">Anmelden</button>
+             </form>
+            </section>
+   
+              
+    
+    <script>
+        function Alert() {
+            var email= document.getElementById("email").value;
+            var pass=document.getElementById("pass").value;
+            
+            if (email=="") {
+                document.getElementById("alert").innerHTML="Bitte E-Mail Adresse eingeben";
+                document.getElementById("alert").style.color="red";
+                document.getElementById("alert").style.fontFamily='poppins', sans-serif;
+            }
+            if (pass=="") {
+                document.getElementById("alert").innerHTML="Bitte  Password eingeben";
+                document.getElementById("alert").style.color="red";
+                document.getElementById("alert").style.fontFamily='poppins', sans-serif;
+            }
+        }
+        function hold() {
+            document.getElementById("alert").innerHTML="";         
+        }
+    </script>
+       
+    
+</body>
+</html>
